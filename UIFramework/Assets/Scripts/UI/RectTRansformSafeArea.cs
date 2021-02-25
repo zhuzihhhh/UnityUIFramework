ddcb44c2bb63339412522da2b5c058ea390e1d77
zhaofeng <53897179@qq.com>
2021-02-25 11:25:23 +0800

    add dotween pro

---
+++ UIFramework/Assets/Scripts/UI/RectTRansformSafeArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RectTRansformSafeArea : MonoBehaviour {
+    void Start() {
+        RectTransform rect = transform as RectTransform;
+        var area = Screen.safeArea;
+        var resolition = Screen.currentResolution;
+
+        if (rect == null) return;
+        rect.anchorMax = new Vector2(area.xMax / Screen.width, area.yMax / Screen.height);
+        rect.anchorMin = new Vector2(area.xMin / Screen.width, area.yMin / Screen.height);
+    }
+}
