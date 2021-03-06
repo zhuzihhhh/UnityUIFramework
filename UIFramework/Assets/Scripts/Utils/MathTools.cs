c29fb23acd3e2ea59d7bbbe308b274293bf788df
Zhao feng <53897179@qq.com>
2021-03-06 17:30:12 +0800

    decomposite a int to a int array
    把一个整数分解成一个整数数组，这个数的和等于这个整数

---
+++ UIFramework/Assets/Scripts/Utils/MathTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public static class MathTools {
+    /// <summary>
+    /// 给出一个int值，给出一个数组长度值，返回一个数组，这个数组的和等于这个int值
+    /// 如果长度为0，或者长度大于sum本身，都返回一个长度为一的数组，唯一的元素就是sum本身
+    /// </summary>
+    /// <param name="sum">给出的数组的和，</param>
+    /// <param name="length">指定数组的长度</param>
+    /// <returns></returns>
+    public static int[] GetArrayBySum(int sum, int length) {
+        if (length == 0 || sum <= length) return new[] {sum};
+
+        int average = sum / length;
+        int remainder = sum % length;
+
+        int[] result = new int[length];
+        for (var i = 0; i < result.Length; i++) {
+            result[i] = average;
+        }
+
+        result[result.Length - 1] += remainder;
+        return result;
+    }
+}
---
+++ UIFramework/Assets/Scripts/Utils/UtilsTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UtilsTest : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        int sum = 100;
+        int length = 10;
+        int length2 = 7;
+        int length3 = 100;
+        int length4 = 120;
+
+        int length5 = 1;
+
+        var r1 = MathTools.GetArrayBySum(sum, length);
+        var r2 = MathTools.GetArrayBySum(sum, length2);
+        var r3 = MathTools.GetArrayBySum(sum, length3);
+        var r4 = MathTools.GetArrayBySum(sum, length4);
+        var r5 = MathTools.GetArrayBySum(sum, length5);
+        Print(r1);
+        Print(r2);
+        Print(r3);
+        Print(r4);
+        Print(r5);
+    }
+    
+
+    void Print(int[] arr) {
+        for (int i = 0; i < arr.Length; i++) {
+            print(arr[i]);
+        }
+        print("----------");
+    }
+}
